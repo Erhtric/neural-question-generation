3f799c1b7615481b383e8212136202f05a135ccb
ericrossetto <97rio.eric@gmail.com>
2022-07-06 12:39:38 +0200

    added comments to utils

--- utils/utils.py
+++ utils/utils.py
@@ -16,6 +16,15 @@ def get_args():
     return args
 
 def prepare_embeddings(word_to_idx_context, word_to_idx_question):
+    """Creates the embedding matrix for the context and question words by using the pretrained GloVe embeddings.
+
+    Args:
+        word_to_idx_context (list): list containing the word to index mappings for the context words for the split dataset.
+        word_to_idx_question (list): list containing the word to index mappings for the question words for the split dataset.
+
+    Returns:
+        the embedding matrices for the context and question words.
+    """
     # Initalize the handler for GloVe
     glove_handler = GloVe(embedding_dimension=model_config['embedding_dimension'])
 
@@ -30,10 +39,18 @@ def prepare_embeddings(word_to_idx_context, word_to_idx_question):
     return embedding_matrix_context, embedding_matrix_question
 
 # Utility function in order to build the compiled model
-def build_trainer(model_config,
-                embedding_matrix_context,
-                embedding_matrix_question,
-                compile_info):
+def build_trainer(model_config, embedding_matrix_context, embedding_matrix_question, compile_info):
+    """Builds the model for the model configuration and compiling information given.
+
+    Args:
+        model_config (dict): dictionary containing the model configuration
+        embedding_matrix_context (numpy.array): embedding matrix for the context words.
+        embedding_matrix_question (numpy.array): embedding matrix for the question words.
+        compile_info (dict): dictionary containing the compile information for the model.
+
+    Returns:
+        model: compiled Keras model
+    """
     model = Trainer(model_config,
                   embedding_matrix_context=embedding_matrix_context,
                   embedding_matrix_question=embedding_matrix_question)
@@ -63,6 +80,13 @@ def train_model(model,
     return history, model
 
 def plot_history(hist):
+    """Plot the history values resulting from the training. The history is a dictionary with the following keys:
+    1. loss
+    2. accuracy and masked accuracy
+    3. perplexity
+
+    both from the validation and the test sets.
+    """
     fig, axs = plt.subplots(1, 3, figsize=(20,5))
 
     fig.suptitle('Training trends')
